a5f4605f51cd1b1aafddd0722a61181b5b18c710
JasperJames-j <galang.jasperjames.guianan@gmail.com>
2024-07-05 09:08:28 +0000

    practice

--- assignments/activity2.java
+++ assignments/activity2.java
@@ -24,16 +24,17 @@ public class activity2 {
         
         System.out.println("The three numbers are: " + num1 +" | " + num2 +" | " + num3);
 
-        if (num1 > num2 && num1 > num3) {
+        if (num1 == num2 && num1 == num3 && num2 == num1 && num2 == num3 && num3 == num1 && num3 == num2) {
+            System.out.println("All numbers are equal"); 
+        }
+        else if (num1 >= num2 && num1 >= num3) {
             System.out.println("The largest number is: " + num1);
         }
-        else if (num2 > num1 && num2 > num3) {
+        else if (num2 >= num1 && num2 >= num3) {
             System.out.println("The largest number is: " + num2);
         }
-        else if (num3 > num1 && num3 >num2) {
+        else if (num3 >= num1 && num3 >=num2) {
             System.out.println("The largest number is: " + num3);
         }
-        else
-            System.out.println("All numbers are equal");
     }
 }
---
+++ works/arrCanapi.java
@@ -0,0 +1,17 @@
+package works;
+
+import java.util.Scanner;
+
+public class arrCanapi {
+    public static void main(String[] args) {
+        //                  0         1         2      3       4
+        Scanner s = new Scanner(System.in);
+        String names[][] = {
+            {"Jover", "Francis", "Doc", "Rica", "Jay"},
+            {"Val", "Jasper", "Doc", "Rica", "Jay"}
+        };
+
+        
+
+    }
+}
